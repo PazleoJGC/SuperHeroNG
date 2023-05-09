1b5c4b57c5ac181248dc18a95a17b2a1313c0073
PazleoJGC <pazdurleon@gmail.com>
2023-05-09 12:20:33 +0200

    Added CRUD operations on the backend

--- SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -21,5 +21,43 @@ namespace SuperHeroAPI.Controllers
         {
             return Ok(await _context.SuperHeroes.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return await GetSuperHeroes();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero == null)
+                return BadRequest("Hero not found.");
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return await GetSuperHeroes();
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+            if (dbHero == null)
+                return BadRequest("Hero not found.");
+
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return await GetSuperHeroes();
+        }
     }
 }
