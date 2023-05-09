3bbf9c3a0fa76e4f92052031d32994341a07d0fe
PazleoJGC <pazdurleon@gmail.com>
2023-05-09 12:04:57 +0200

    Added database connection

--- SuperHeroAPI/SuperHeroAPI/Program.cs
+++ SuperHeroAPI/SuperHeroAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
 namespace SuperHeroAPI
 {
     public class Program
@@ -13,6 +16,10 @@ namespace SuperHeroAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
                 policy =>
                 {
